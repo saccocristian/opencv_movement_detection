08ae89236101ccc356dd655e4e84595960a468cc
Cristian Sacco <cristiansacco@icloud.com>
2026-03-05 14:14:36 +0100

    aggiunto salvataggio video per rilevamento persone

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+from xxlimited_35 import Null
+
 import cv2
 import time
 import os
 from datetime import datetime
 
+def mkdirectory(path):
+    try:
+        os.makedirs(path)
+        print(f"Nested directories '{path}' created successfully.")
+    except FileExistsError:
+        print(f"One or more directories in '{path}' already exist.")
+    except PermissionError:
+        print(f"Permission denied: Unable to create '{path}'.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def fine_registrazione(prev_timestamp, now_timestamp):
+    # TRUE: se timestamp attuale - timestamp salvato maggiore del tempo fissato, esco dal ciclo
+    # FALSE: Altrimenti
+    recording_minutes = 1
+    return now_timestamp - prev_timestamp > recording_minutes * 60
+    return
 
 def main():
+    # apertura camera
     cap = cv2.VideoCapture(0)
     first_frame = None
 
+    # Informazioni dimensioni cattura video
+    frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+    #Variabili controllo per salvataggio files
+    movimento_rilevato = False
+    salvataggio_video = False
+    prev_timestamp = 0
+
     print("Sistema pronto. Premi 'r' per resettare lo sfondo, 'q' per uscire.")
+
+    for i in range (2):
+        print("Avvio in corso ...", i)
+        time.sleep(1)
 
     while True:
         ret, frame = cap.read()
@@ -16,7 +49,6 @@ def main():
             break
 
         # --- PRE-PROCESSING ---
-
         # Lavoro in scala di grigi
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (21, 21), 0)
@@ -31,12 +63,9 @@ def main():
         _, thresh = cv2.threshold(delta, 25, 255, cv2.THRESH_BINARY)
         thresh = cv2.dilate(thresh, None, iterations=4)
 
-        # Trovo tutti i contorni sulla differenza dei pixel; non e' piu' necessario copiare l'oggetto,
-        # ma e' buona abitudine farlo
+        # Trovo tutti i contorni sulla differenza dei pixel;
+        # non e' piu' necessario copiare l'oggetto, ma e' buona abitudine farlo
         contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        movimento_rilevato = False
-        salvataggio_video = False
 
         for contour in contours:
             movimento_rilevato = True
@@ -55,29 +84,34 @@ def main():
             cv2.putText(frame, "Movimento Rilevato!", (x, y - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
-        # Salviamo video relativo a movimento
+        # Salvo la data e la converto in timestamp
+        my_datetime = datetime.now()
+        timestamp = my_datetime.timestamp()
+
+        # Creo ambiente per salvataggio video
         if movimento_rilevato and not salvataggio_video:
             salvataggio_video = True
-            # Salvo la data, la converto in timestamp e ottengo strighe di data e ora per le cartelle
-            my_datetime = datetime.now()
-            timestamp = my_datetime.timestamp()
+            prev_timestamp = timestamp
+            # Ottengo strighe di data e ora per le cartelle
             my_date = str(my_datetime)[0:10].replace("-","_")
             my_time = str(my_datetime)[11:19].replace(":","_")
             print(my_date)
             print(my_time)
-            # Creo cartella con il giorno, poi la creo con l'ora (se non esistono)
-            nested_directory = my_date + "/" + my_time
 
-            # Create nested directories
-            try:
-                os.makedirs(nested_directory)
-                print(f"Nested directories '{nested_directory}' created successfully.")
-            except FileExistsError:
-                print(f"One or more directories in '{nested_directory}' already exist.")
-            except PermissionError:
-                print(f"Permission denied: Unable to create '{nested_directory}'.")
-            except Exception as e:
-                print(f"An error occurred: {e}")
+            # Creo cartella con il giorno, poi la creo con l'ora (se non esistono)
+            nested_directory = "recordings/" + my_date + "/" + my_time
+            mkdirectory(nested_directory)
+            out = cv2.VideoWriter(nested_directory + "/output.mp4",cv2.VideoWriter.fourcc(*'mp4v'),20,(frame_width,frame_height))
+
+        # Salvataggio video
+        if salvataggio_video:
+            out.write(frame)
+            # se finisce il tempo della registrazione, resetto variabili e interrompo salvataggio video
+            if fine_registrazione(prev_timestamp,timestamp):
+                out.release()
+                prev_timestamp = 0
+                salvataggio_video = False
+                continue
 
         # Visualizziamo i risultati
         cv2.imshow("Originale con Bounding Box", frame)
